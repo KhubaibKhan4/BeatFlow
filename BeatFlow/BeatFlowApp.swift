66232cf7725c4a0fc29362e31fd56f8e2b852869
KhubaibKhan4 <18.bscs.803@gmail.com>
2025-02-01 13:35:56 +0500

    Initial Commit

---
+++ BeatFlow/BeatFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatFlowApp.swift
+//  BeatFlow
+//
+//  Created by Muhammad Khubaib Imtiaz on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeatFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
